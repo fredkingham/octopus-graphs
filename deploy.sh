80a311610aff9859b403e3c0726a3e7f6e625b49
fredkingham <fred.kingham@openhealthcare.org.uk>
2021-03-24 18:01:02 +0000

    Improvements to the deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 git checkout -b deployment
 git checkout deployment
-git merge -
+git merge -m "merged" -
 npm run build
 git add -f dist
 git commit -m "gh-pages-bump"
 git subtree push --prefix dist origin gh-pages-4
 git checkout -
+echo 'deployed to https://fredkingham.github.io/octopus-graphs'
