8b3963fd08a63ceda6a33f46968c4a4efc365006
fredkingham <fred.kingham@openhealthcare.org.uk>
2021-12-29 21:41:25 +0000

    Pad out the dates in graph so that they look like dates

--- src/services/data_to_graph.js
+++ src/services/data_to_graph.js
@@ -1,6 +1,25 @@
+import _ from 'lodash'
+
+
+let numDaysLeft = function(lastDateWithData){
+  /*
+  * Returns the number of days left in the month after the given date.
+  */
+  let someDate = new Date(lastDateWithData.getFullYear(), lastDateWithData.getMonth() + 1, 0)
+  let numDaysInMonth = someDate.getDate();
+  return numDaysInMonth - lastDateWithData.getDate();
+}
+
+
 let translate = function(xtitle, someData, max) {
   let config = {};
-  let data = someData.results.map((x) => x.consumption);
+  let results = someData.results;
+  _.each(results, result => {
+    result.interval_start = new Date(result.interval_start);
+  })
+  results = _.orderBy(results, 'interval_start');
+
+  let data = results.map((x) => x.consumption);
 
   // lets make graphs consistent
   data.forEach(x => {
@@ -9,13 +28,23 @@ let translate = function(xtitle, someData, max) {
     }
   })
   data.unshift(xtitle);
-  let xAxis = someData.results.map((x) => {
+  let xAxis = results.map((x) => {
     let asDate = new Date(x.interval_start);
     asDate.setMilliseconds(0);
     asDate.setSeconds(0);
     asDate.setHours(0);
     return asDate;
   });
+
+  // Pad out the month to the full month
+  if(xAxis.length){
+    var lastDate = xAxis[xAxis.length -1];
+    var numDaysLeftInMonth = numDaysLeft(lastDate);
+    _.each(_.range(numDaysLeftInMonth), amount => {
+      xAxis.push(new Date(lastDate.getFullYear(), lastDate.getMonth(), lastDate.getDate() + amount))
+      data.push(null);
+    })
+  }
 
   xAxis.unshift("x");
   config = {
